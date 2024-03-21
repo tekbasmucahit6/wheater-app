472b5db2a581efe35d04d6f96346d9bcfd67d8b0
mücahit tekbaş <tekbasmucahit6@gmail.com>
2024-03-21 23:23:19 +0300

    search bar düzeltmeler

--- src/components/Wheater.jsx
+++ src/components/Wheater.jsx
@@ -7,15 +7,6 @@ function Wheater() {
     const navigate = useNavigate();
     const [city,setCity] = useState();
 
-    const searchable = () => {
-        if(city  != ""){
-            navigate(`/search/${city}`)
-        }
-        else{
-            console.log("lütfen veri giriniz")
-            navigate("/")
-        }
-    }
     
   
     return (
@@ -26,7 +17,7 @@ function Wheater() {
     <div className=' mt-10 flex w-full place-content-center'>
         <div className='grid grid-cols-2 gap-3'>
             <input onChange={(e) => setCity(e.target.value)} type="text" placeholder='şehir ismi giriniz' className='p-2 bg-transparent outline-none rounded-2xl border border-red-800' />
-            <button  onClick={searchable} className='bg-red-800 w-20 text-center text-white flex justify-center items-center p-2 rounded-xl'><FaSearch className={"font-3xl"}/></button>
+            <button  onClick={() => navigate(`/search/${city}`)} className='bg-red-800 w-20 text-center text-white flex justify-center items-center p-2 rounded-xl'><FaSearch className={"font-3xl"}/></button>
         </div>
     </div>
         </>
